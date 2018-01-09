4412de64b99bda0d7fe629e957c275c0e464545b
weijizhu1000 <630627196@qq.com>
2018-01-09 09:07:14 +0800

    重构代码

--- server/routes/restful/upload.api.js
+++ server/routes/restful/upload.api.js
@@ -1,20 +1,7 @@
-var multer = require('multer')
-var path = require('path')
-var config = require('../../constant/config')
 var FileDb = require('../../db/mongo/index').File
+var upload = require('../../common/fileuploads')
 
 var ResponseResult = require('../model/response.result')
-
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, "../../../" + config.upload_dir))
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
-})
-var multer = multer({ storage: storage })
-var upload = multer.single('file')
 
 module.exports = {
     init: function (app, auth) {
